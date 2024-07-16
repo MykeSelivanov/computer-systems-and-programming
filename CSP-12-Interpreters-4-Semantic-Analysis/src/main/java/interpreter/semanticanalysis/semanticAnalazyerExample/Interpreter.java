856124380ad11ce1e0b34c59470beeeb12afc511
MykeSelivanov <michael777selivanov@gmail.com>
2024-07-15 22:27:44 -0500

    interpreter

---
+++ CSP-12-Interpreters-4-Semantic-Analysis/src/main/java/interpreter/semanticanalysis/semanticAnalazyerExample/Interpreter.java
@@ -0,0 +1,48 @@
+package interpreter.semanticanalysis.semanticAnalazyerExample;
+
+import interpreter.semanticanalysis.semanticAnalazyerExample.ast.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Interpreter {
+    private final Map<String, Integer> valueTable = new HashMap<>();
+
+    int visit(ASTNode node) {
+        if (node instanceof BinaryOpNode binaryOpNode) {
+            int left = visit(binaryOpNode.left);
+            int right = visit(binaryOpNode.right);
+            switch (binaryOpNode.operationToken.type) {
+                case MINUS -> { return left - right; }
+                case PLUS -> { return left + right; }
+                case DIVIDE -> { return left / right; }
+                case MULTIPLY -> { return left * right; }
+                default -> throw new InterpreterException("Unexpected token: " + binaryOpNode.operationToken);
+            }
+        } else if (node instanceof NumberNode numberNode) {
+            return numberNode.value;
+        } else if (node instanceof VarDecl varDecl) {
+            int rightExpressionResult = visit(varDecl.expr);
+            valueTable.put(varDecl.varNode.name, rightExpressionResult);
+            return rightExpressionResult;
+        } else if (node instanceof Var var) {
+            String varName = var.name;
+            if (!valueTable.containsKey(varName)) {
+                throw new InterpreterException("Variable not found: " + varName);
+            }
+            return valueTable.get(varName);
+        } else if (node instanceof Assign assignNode) {
+            int rightExpressionResult = visit(assignNode.right);
+            valueTable.put(assignNode.left.name, rightExpressionResult);
+            return rightExpressionResult;
+        } else if (node instanceof Block block) {
+            int result = 0;
+            for (ASTNode statement: block.statements) {
+                result = visit(statement);
+            }
+            return result;
+        } else {
+            throw new InterpreterException("Unexpected AST Node: " + node.getClass().getCanonicalName());
+        }
+    }
+}
