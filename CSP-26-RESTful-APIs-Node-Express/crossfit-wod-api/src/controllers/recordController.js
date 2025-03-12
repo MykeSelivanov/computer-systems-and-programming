4926b5dd723a21bffec7cf70198f29910111b652
MykeSelivanov <michael777selivanov@gmail.com>
2025-03-11 22:48:50 -0500

    restful apis

---
+++ CSP-26-RESTful-APIs-Node-Express/crossfit-wod-api/src/controllers/recordController.js
@@ -0,0 +1,26 @@
+const recordService = require("../services/recordService");
+
+const getRecordForWorkout = (req, res) => {
+    const {
+        params: { workoutId },
+    } = req;
+
+    if (!workoutId) {
+        return res.status(400).send({
+            status: "FAILED",
+            data: { error: "Parameter ':workoutId' cannot be empty" },
+        });
+    }
+
+    try {
+        const record = recordService.getRecordForWorkout(workoutId);
+        res.send({ status: "OK", data: record });
+    } catch (error) {
+        res.status(error?.status || 500).send({
+            status: "FAILED",
+            data: { error: error?.message || error },
+        });
+    }
+};
+
+module.exports = { getRecordForWorkout };
